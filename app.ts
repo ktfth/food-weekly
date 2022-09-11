c4247c2505cf9bc889a3f5592cd4d0968a4a6b36
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-09-11 13:34:31 -0300

    add basic crud of meals

--- app.ts
+++ app.ts
@@ -17,14 +17,6 @@ type Meal = {
     happiness: number;
 }
 
-// const meals = new Map<MealId, Meal>();
-// meals.set(1, {
-//   id: 1,
-//   name: "Nhoque",
-//   rating: 3,
-//   happiness: 5
-// });
-
 const router = new Router();
 router
   .get("/", (context) => {
@@ -34,7 +26,8 @@ router
     const body = context.request.body();
     const mealData = await body.value;
     const meals = db.collection<Meal>("meals");
-    const meal = await meals.insertOne(mealData);
+    const _mealCreated = await meals.insertOne(mealData);
+    const meal = await meals.findOne({ _id: _mealCreated });
     context.response.body = {
       success: true,
       data: meal,
@@ -54,6 +47,38 @@ router
     context.response.body = {
       success: true,
       data: meal,
+    };
+  })
+  .patch("/meal/:id", async (context) => {
+    const body = context.request.body();
+    const mealId: MealId = new ObjectId(context?.params?.id);
+    const mealData = await body.value;
+    const meals = db.collection<Meal>("meals");
+    const _mealUpdated = await meals.updateOne({ _id: mealId }, { $set: mealData });
+    const meal = await meals.findOne({ _id: mealId });
+    context.response.body = {
+      success: true,
+      data: meal,
+    };
+  })
+  .put("/meal/:id", async (context) => {
+    const body = context.request.body();
+    const mealId: MealId = new ObjectId(context?.params?.id);
+    const mealData = await body.value;
+    const meals = db.collection<Meal>("meals");
+    const _mealUpdated = await meals.updateOne({ _id: mealId }, { $set: mealData });
+    const meal = await meals.findOne({ _id: mealId });
+    context.response.body = {
+      success: true,
+      data: meal,
+    };
+  })
+  .delete("/meal/:id", async (context) => {
+    const mealId: MealId = new ObjectId(context?.params?.id);
+    const meals = db.collection<Meal>("meals");
+    const mealDeleted = await meals.deleteOne({ _id: mealId });
+    context.response.body = {
+      success: mealDeleted ? true : false,
     };
   });
 
