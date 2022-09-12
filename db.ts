9b1c29e14c4337b9b59c03ae6fdcc1446a8d0ade
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-09-11 22:03:06 -0300

    add config usage for mongo uri

--- db.ts
+++ db.ts
@@ -1,7 +1,10 @@
 import {
     MongoClient,
 } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
+import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+
+const { MONGO_URI } = config();
 
 const client = new MongoClient();
 
-export const db = await client.connect("mongodb://127.0.0.1:27017/food-weekly");
+export const db = await client.connect(MONGO_URI || "mongodb://127.0.0.1:27017/food-weekly");
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { app } from "./app.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+
 const { PORT } = config();
 
 await app.listen({ port: parseInt(PORT, 10) || 8000 });
