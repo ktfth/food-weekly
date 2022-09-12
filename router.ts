79c708cb5da134b84b03917a2ae2f7d7daeba59b
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-09-11 21:55:08 -0300

    add use cases to all possible cases on the app

--- router.ts
+++ router.ts
@@ -1,71 +1,37 @@
 import { Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import { Database, ObjectId } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
-import {
-    MealId,
-    createMealStore,
-    Meal,
-    MealStore,
-    SingleMeal,
-} from './store.ts';
+import { Database } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
+import * as useCase from './use-cases.ts';
+import { MealStore, Meal, createMealStore } from "./store.ts";
 
 export function setupRouter(db: Database) {
-    const mealStore: MealStore<Meal> = createMealStore(db);
     const router = new Router();
 
     router
+        .use(async (context, next) => {
+            const mealStore: MealStore<Meal> = createMealStore(db);
+            context.app.state.mealStore = mealStore;
+            await next();
+        })
         .get("/", (context) => {
-            context.response.body = "Hello from Food Weekly!";
+            context.response.body = useCase.health();
         })
         .post("/meals", async (context) => {
-            const body = context.request.body();
-            const mealData: Meal = await body.value;
-            const meal: SingleMeal = await mealStore.create(mealData);
-            context.response.body = {
-                success: true,
-                data: meal,
-            };
+            context.response.body = await useCase.createMeal({...context});
         })
         .get("/meals", async (context) => {
-            context.response.body = {
-                success: true,
-                data: await mealStore.find({}),
-            };
+            context.response.body = await useCase.findMeals({...context});
         })
         .get("/meal/:id", async (context) => {
-            const mealId: MealId = new ObjectId(context?.params?.id);
-            const meal: SingleMeal = await mealStore.get(mealId);
-            context.response.body = {
-                success: true,
-                data: meal,
-            };
+            context.response.body = await useCase.getMeal({...context});
         })
         .patch("/meal/:id", async (context) => {
-            const body = context.request.body();
-            const mealId: MealId = new ObjectId(context?.params?.id);
-            const mealData: Meal = await body.value;
-            const meal: SingleMeal = await mealStore.update(mealId, { $set: mealData });
-            context.response.body = {
-                success: true,
-                data: meal,
-            };
+            context.response.body = await useCase.updateMeal({...context});
         })
         .put("/meal/:id", async (context) => {
-            const body = context.request.body();
-            const mealId: MealId = new ObjectId(context?.params?.id);
-            const mealData: Meal = await body.value;
-            const meal: SingleMeal = await mealStore.update(mealId, { $set: mealData });
-            context.response.body = {
-                success: true,
-                data: meal,
-            };
+            context.response.body = await useCase.updateMeal({...context});
         })
         .delete("/meal/:id", async (context) => {
-            const mealId: MealId = new ObjectId(context?.params?.id);
-            const meal: SingleMeal = await mealStore.remove(mealId);
-            context.response.body = {
-                success: meal ? true : false,
-                data: meal,
-            };
+            context.response.body = await useCase.deleteMeal({...context});
         });
 
     return router;
---
+++ use-cases.ts
@@ -0,0 +1,57 @@
+import { RouteParams } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { ObjectId } from "https://deno.land/x/web_bson@v0.2.5/mod.ts";
+import {
+    MealId,
+    Meal,
+    SingleMeal,
+} from './store.ts';
+
+export function health() {
+    return { success: true, message: "Hello from Food Weekly!" };
+}
+
+export async function createMeal(context: any) {
+    const body = context.request.body();
+    const mealData: Meal = await body.value;
+    const meal: SingleMeal = await context.app.state.mealStore.create(mealData);
+    return {
+        success: true,
+        data: meal,
+    };
+}
+
+export async function findMeals(context: any) {
+    return {
+        success: true,
+        data: await context.app.state.mealStore.find({}),
+    };
+}
+
+export async function getMeal(context: any) {
+    const mealId: MealId = new ObjectId(context?.params?.id);
+    const meal: SingleMeal = await context.app.state.mealStore.get(mealId);
+    return {
+        success: true,
+        data: meal,
+    };
+}
+
+export async function updateMeal(context: any) {
+    const body = context.request.body();
+    const mealId: MealId = new ObjectId(context?.params?.id);
+    const mealData: Meal = await body.value;
+    const meal: SingleMeal = await context.app.state.mealStore.update(mealId, { $set: mealData });
+    return {
+        success: true,
+        data: meal,
+    };
+}
+
+export async function deleteMeal(context: any) {
+    const mealId: MealId = new ObjectId(context?.params?.id);
+    const meal: SingleMeal = await context.app.state.mealStore.remove(mealId);
+    return {
+        success: meal ? true : false,
+        data: meal,
+    };
+}
